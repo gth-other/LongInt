2244be5cc58a903f4ab39d94bd7d149049058182
gth-other <cheb0tarev.nikit0s@gmail.com>
2021-09-24 18:51:02 +0300

    Исправлена критическая ошибка в реализации алгоритма Анатолия Алексеевича Карацубы. Версия считается стабильной.

--- LongInt.cpp
+++ LongInt.cpp
@@ -385,8 +385,8 @@ LongInt LongInt::_multiply_karatsuba(LongInt number_first, LongInt number_second
     LongInt product_second;
     LongInt product_third;
     if (iteration_thirst == true) {
-        auto thread_first = std::async(LongInt::_multiply_karatsuba, number_first_part_left, number_first_part_right, false);
-        auto thread_second = std::async(LongInt::_multiply_karatsuba, number_second_part_left, number_second_part_right, false);
+        auto thread_first = std::async(LongInt::_multiply_karatsuba, number_first_part_left, number_second_part_left, false);
+        auto thread_second = std::async(LongInt::_multiply_karatsuba, number_first_part_right, number_second_part_right, false);
         product_third = LongInt::_multiply_karatsuba(LongInt::_zeroes_leading_remove(number_first_part_left) + LongInt::_zeroes_leading_remove(number_first_part_right), LongInt::_zeroes_leading_remove(number_second_part_left) + LongInt::_zeroes_leading_remove(number_second_part_right), false);
         product_first = thread_first.get();
         product_second = thread_second.get();
